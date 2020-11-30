f249e184f4f111fbf395186fd12aba8e9f21831f
shahdmadhoun <shahdem100@gmail.com>
2020-11-30 19:17:32 +0300

    reduceMethod

--- js.js
+++ js.js
@@ -5,20 +5,20 @@ let tagsUppercased = tags.map(tags => tags.toUpperCase());
 document.getElementById("map").innerHTML = "the tags with uppercase : " + tagsUppercased;
 
 
-const filteredItems = tags.filter(tags => tags !== "xml" && tags !== "json")
+let filteredItems = tags.filter(tags => tags !== "xml" && tags !== "json")
 document.getElementById("filter").innerHTML = "the tags with Exclude all xml and json tags: " + filteredItems;
 
 
-var wordCountsArray = inputWords.map(function(item){
+var wordCountsArray = filteredItems.map(function(item){
     var counter = 0;
-    var itemCount = inputWords.reduce(function(prevVal, curVal){
+    var itemCount = filteredItems.reduce(function(prevVal, curVal){
         if(curVal==item){
             counter++;
         }
-        return;
+        return item+": "+counter;
     },0);
 
     return item+": "+counter;
 })
 
-document.getElementById("reduceMethod").innerHTML = " ";
+document.getElementById("reduceMethod").innerHTML = wordCountsArray;
